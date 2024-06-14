5318efd0b5fec1a82b72884c42178cc36704185c
Cecchy2 <dariocecchinato@gmail.com>
2024-06-14 14:27:10 +0200

    recuperato id da singolo oggetto

--- details.js
+++ details.js
@@ -1,3 +1,23 @@
-const params = new URLSearchParams(window.GeolocationPosition.search);
+const params = new URLSearchParams(window.location.search);
 
-const id = params.get("objectID");
+const id = params.get("objectId");
+
+console.log("Id Pagina:", id);
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("https://striveschool-api.herokuapp.com/api/product/", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjlhOTdjMjM5YzAwMTUyZjRiM2QiLCJpYXQiOjE3MTgzNTIyOTgsImV4cCI6MTcxOTU2MTg5OH0.y7gHg1PKQ4OQXp0Hab01t6w5m7V4aSUl2Lau-YghrEo",
+    },
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("ERRORE FETCH");
+      }
+    })
+    .then((singleObj) => {})
+    .catch((err) => console.log(err));
+});
