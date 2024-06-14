797c4736b4f630cb7975ec0094810c08cb470754
Cecchy2 <dariocecchinato@gmail.com>
2024-06-14 15:18:01 +0200

    Ripopolati input nel caso di modifica e creata funzione per bottone delete e modifica

--- backOffice.js
+++ backOffice.js
@@ -1,13 +1,58 @@
 const id = new URLSearchParams(window.location.search).get("objectId");
 
+console.log("RESOURCE ID", id);
+
+const URL = id
+  ? "https://striveschool-api.herokuapp.com/api/product/" + id
+  : "https://striveschool-api.herokuapp.com/api/product/";
+
 const authorization =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjlhOTdjMjM5YzAwMTUyZjRiM2QiLCJpYXQiOjE3MTgzNTIyOTgsImV4cCI6MTcxOTU2MTg5OH0.y7gHg1PKQ4OQXp0Hab01t6w5m7V4aSUl2Lau-YghrEo";
 
-const URL = "https://striveschool-api.herokuapp.com/api/product/";
 window.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
 
   form.onsubmit = formFunct;
+
+  if (id) {
+    const form = document.querySelector("form");
+
+    const textChange = document.getElementById("CreaOModifica");
+    textChange.innerText = "MODIFICA OGGETTO";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn btn-danger";
+    deleteBtn.innerText = "cancella il prodotto";
+    deleteBtn.type = "button";
+    deleteBtn.onclick = deleteProduct;
+
+    const submitBtn = document.getElementById("backOfficeBtn");
+    submitBtn.innerText = "Modifica";
+
+    form.appendChild(deleteBtn);
+
+    fetch(URL, {
+      headers: {
+        Authorization: authorization,
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("ERRORE");
+        }
+      })
+      .then((singleObj) => {
+        const { name, description, brand, imageUrl, price } = singleObj;
+        document.getElementById("inputName").value = name;
+        document.getElementById("description").value = description;
+        document.getElementById("brand").value = brand;
+        document.getElementById("imageUrl").value = imageUrl;
+        document.getElementById("price").value = price;
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 const formFunct = (event) => {
@@ -21,7 +66,7 @@ const formFunct = (event) => {
   };
 
   fetch(URL, {
-    method: "POST",
+    method: id ? "PUT" : "POST",
     body: JSON.stringify(newObject),
     headers: {
       "Content-Type": "application/json",
@@ -33,11 +78,30 @@ const formFunct = (event) => {
       if (resp.ok) {
         return resp.json();
       } else {
-        throw new Error("ERRORE NELLA CREAZIONE DELL'OGGETTO");
+        throw new Error("Error creating/updating the object");
       }
     })
-    .then((createObject) => {})
+    .then((createObject) => {
+      console.log("Object created/updated successfully", createObject);
+      // Optional: redirect or clear form
+    })
     .catch((err) => console.log(err));
+};
 
-  console.log(createObject);
+const deleteProduct = () => {
+  fetch(URL, {
+    method: "DELETE",
+    headers: {
+      Authorization: authorization,
+    },
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        console.log("PRODOTTO ELIMINATO");
+        // Optional: redirect or notify user
+      } else {
+        throw new Error("ERRORE NELLA CANCELLAZIONE");
+      }
+    })
+    .catch((err) => console.log(err));
 };
